e8da27eb2468347f98fdbe5845d377c762fc3f68
Brian Chenoweth <brian.michael.chenoweth@gmail.com>
2026-06-04 19:10:31 -0700

    add blank line between import groups

--- components/SEO/SEO.js
+++ components/SEO/SEO.js
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import { useRouter } from 'next/router';
+
 import { getPublicSiteOrigin } from '../../utilities/normalizeInternalLink';
 
 const DEFAULT_SOCIAL_IMAGE_PATH = '/static/banner.jpeg';
